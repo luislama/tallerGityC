aec94703ac71c6a65e6eb0f92602acd87637f03e
luislama <luislama@espol.edu.ec>
2017-05-26 13:13:33 -0500

    Validar la entrada de un numero entero

--- parte1.c
+++ parte1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TAMANO 1024
 
@@ -6,7 +7,8 @@ char texto[TAMANO]={0};
 
 int main()
 {
-   char str2[30]={0};
+	char llave[30]={0};
+	int llaveNum=0;
 
 	printf("\nCifrado Cíclico");
 
@@ -14,12 +16,18 @@ int main()
 	scanf("%1024[^\n]", texto);
 
  	printf("\n\nIngrese la llave numérica: ");
-	scanf("%s", str2);
+	scanf("%s", llave);
+	llaveNum = atoi(llave);
+
+	if(llaveNum==0)
+		printf("\n\nLlave numérica incorrecta!");
+	else
+		printf("\n\nLlave numérica correcta!");
 
 	printf("\n");
 	printf("\n\nMensaje: %s", texto);
-	printf("\n\nLlave numérica: %s", str2);
+	printf("\n\nLlave numérica: %s", llave);
 	printf("\n");
-   
+
    return(0);
 }

6e0d53effe0c449e602e97afb7be033dd995af2d
luislama <luislama@espol.edu.ec>
2017-05-26 16:54:51 -0500

    Parte 1 Completa, funciona para positivos, negativos, y llaves que se salgan del rango

--- parte1.c
+++ parte1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 void userInput();
 
-void printCifrado(char *mensaje, int *num);
-/*void ciclico(char *mensaje, int *num);*/
+void printCifrado();
+void ciclico();
 
 #define TAMANO 1024		/*tamaño del mensaje*/
 
 char texto[TAMANO] = {0};	/*variable donde se almacenara el mensaje*/
 int llaveNum = 0;		/*variable donde se almacenara la llave numerica*/
-int ingreso = 0;		/*guardara 1 si el ingreso es valido, 0 si no*/
 
 int main()
 {
 	userInput();
 	
-	printCifrado(texto,&llaveNum);
+	printCifrado();
 
 	return(0);
 }
@@ -30,64 +30,54 @@ void userInput()
 
         printf("\n\nIngrese mensaje a cifrar: ");
         scanf("%1024[^\n]", texto);
-
-        printf("\n\nIngrese la llave numérica: ");
-        scanf("%s", llave);
-        llaveNum = atoi(llave);
-
-        if(llaveNum==0)
-		printf("\n\nLlave numérica incorrecta!");
-        else
-        {
-                printf("\n\nLlave numérica correcta!");
-		ingreso = 1;
-        }
-
-}
-
-void printCifrado(char *mensaje, int *num)
-{
-	printf("\n");
-        printf("\n\nMensaje: %s", mensaje);
-        printf("\n\nLlave numérica: %i", *num);
-        printf("\n");
-}
-/*
-void ciclico(char *mensaje, int *num)
-{
-	char cifrado[1024];
-	int c="";
-	char letra="";
-	if(num>26)
-		int llave = num%26;
-	while( EOF != (c = getchar()) )
+	
+	while(llaveNum == 0)
 	{
+	        printf("\nIngrese la llave numérica: ");
+        	scanf("%s", llave);
+	        llaveNum = atoi(llave);
+		if(llaveNum == 0)
+			 printf("\n\nLlave numérica incorrecta!");
+	}
+	if(llaveNum>26 || llaveNum<-26)
+		llaveNum %= 26;
+	if(llaveNum<0)
+		llaveNum += 26;
+}
+
+void printCifrado()
+{
+	printf("\nMensaje Cifrado: ");
+	ciclico();
+        printf("\n\n");
+}
+
+void ciclico()
+{
+	int c=0;
+	char letra = 'a';
+
+	for(int i = 0; i <= strlen(texto); i++) 
+	{
+		c = texto[i];
 		if(c<=90 && c>=64)
 		{
-			if((c+llave)<=90)
-			{
-				letra = c + llave;
-				printf("%s",letra)
-			}
+			if((c+llaveNum)<=90)
+				letra = c + llaveNum;
 			else
-			{
-				letra = c+llave-90+64-1;
-			}
+				letra = 63+(c+llaveNum-90);
 		}
 		else if(c<=122 && c>=97)
 		{
-                        if((c+llave)<=122)
-                        {
-                                letra = c + llave;
-                                printf("%s",letra)
-                        }
+                        if((c+llaveNum)<=122)
+                                letra = c + llaveNum;
                         else
-                        {
-                                letra = c+llave-122+97-1;
-                        }
+                                letra = 96+(c+llaveNum-122);
                 }
-
+		else 
+		{
+			letra = c;
+		}
+		printf("%c",letra);
 	}
-	
 }
-*/

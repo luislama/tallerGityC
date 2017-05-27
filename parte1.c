bd27a6f3d8f632ea08c52a6076545fde23aa2b2f
luislama <luislama@espol.edu.ec>
2017-05-26 19:47:22 -0500

    ya recibe los parametros y actua dependiendo del numero de parametros y el formato en que son ingresados, falta cifrar el mensaje..

--- parte1.c
+++ parte1.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
+void printMessageError(char *name);
 
 void userInput();
-
-void printCifrado();
 void ciclico();
 
+void printCifrado();
+
+
+
 #define TAMANO 1024		/*tamaño del mensaje*/
-
 char texto[TAMANO] = {0};	/*variable donde se almacenara el mensaje*/
-int llaveNum = 0;		/*variable donde se almacenara la llave numerica*/
 
-int main()
+
+int llaveNum = 0;		/*variable donde se almacenara la llave numerica*/
+int argumentos = 1;
+
+
+int main(int argc, char *argv[])
 {
-	userInput();
-	
-	printCifrado();
+	argumentos = argc;
+	if(argumentos == 1)
+	{
+		userInput();
+		printCifrado();
+	}else if(  argumentos >=3  &&  atoi(argv[1])  )
+	{
+		printf("El programa ha recivido %d argumento(s)\n",argc);
+	        for(int i=0; i<argc; i++)
+        	{
+			printf("%s\n",argv[i]);
+        	}
+	}else
+	{
+		printMessageError(argv[0]);
+	}
 
 	return(0);
+}
+
+void printMessageError(char *name)
+{
+	printf("El programa se lo utiliza de la siguiente manera.\n");
+        printf("-con parametros:\n");
+        printf("%s integer cadenaN1 cadenaN2 ... cadenaNi; Ni>0\n",name);
+        printf("-sin parametros:\n");
+        printf("%s\n\n",name);
 }
 
 void userInput()
@@ -43,6 +72,20 @@ void userInput()
 		llaveNum %= 26;
 	if(llaveNum<0)
 		llaveNum += 26;
+}
+
+void validarLlave(char num)
+{
+
+        llaveNum = atoi(&num);
+
+	if(isdigit(llaveNum))
+	{
+        	if(llaveNum>26 || llaveNum<-26)
+                	llaveNum %= 26;
+	        if(llaveNum<0)
+        	        llaveNum += 26;
+	}
 }
 
 void printCifrado()
